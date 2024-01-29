a6c4c4f18b47048932699d43dfa5d07ab0b0ea2b
Ayrton Vargas <ayrtonvargas17@gmail.com>
2024-01-29 20:36:29 -0300

    contacto js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+async function equipos(params) {
+    const url = 'https://api-football-v1.p.rapidapi.com/v3/timezone';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4d010832d7msh701bc29d258dd79p1d3bf3jsnbdee7a34a9ac',
+		'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+	}
+    };
+    try {
+    	const response = await fetch(url, options);
+    	const result = await response.text();
+    	console.log(result);
+        } catch (error) {
+	        console.error(error);
+        }
+}
+alert("Hola")
+    document .getElementById('btnequipos').addEventListener('click', () => {
+        console.log("click")
+    })
+/*  */
 $(document).ready(function() {    
 
     $("#transition-timer-carousel").on("slide.bs.carousel", function(event) {
